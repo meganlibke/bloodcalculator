de37ca5f2c4d78263e03a2c741c2a099539e1232
Megan Libke <meganlibke@MacBook-Pro.attlocal.net>
2020-08-24 12:54:47 -0400

    Improved Readability

--- interface.py
+++ interface.py
@@ -1,7 +1,7 @@
 def interface():
     print("My Program")
     while True:
-        print("Options:")
+        print("Options for you:")
         print("9 - Quit")
         choice = input("Enter your choice: ")
         if choice=='9':
